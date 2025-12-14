76493fc86aa21c3d3d34617094055add8dbee945
Bryne <228299496+b1yn3@users.noreply.github.com>
2025-12-14 17:48:45 +0800

    Finished

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ let prev_num = '';
 let operation = '';
 let dotBtn = document.querySelector("#dot-btn");
 
+const prevNumDisplay = document.querySelector("#prev-num-inp");
+const currNumDisplay = document.querySelector("#current-num-inp");
+
 document.querySelectorAll(".num").forEach(btn => {
     btn.addEventListener('click', () => {
         temp_num += btn.textContent;
-        console.log(temp_num);
+        updateDisplay();
     })
+})
+
+document.addEventListener('keydown', (event) => {
+    if(event.key >= '0' && event.key <= '9'){
+        temp_num += event.key;
+        updateDisplay();
+    }
 })
 
 dotBtn.addEventListener('click', () => {
     temp_num += '.'
     dotBtn.disabled = true;
+    updateDisplay();
 })
 
 function secondNumInput(){
@@ -21,12 +32,15 @@ function secondNumInput(){
     dotBtn.disabled = false;
 }
 
-// Operations
+// Update
 
-function dot(){
-    temp_num += '.'
-    dotBtn.disabled = true;
+function updateDisplay(){
+    currNumDisplay.textContent = temp_num || '0';
+    prevNumDisplay.textContent = prev_num ? `${prev_num} ${operation}` : '';
 }
+
+
+// Operations
 
 function add(){
     secondNumInput();
@@ -50,6 +64,15 @@ function divide(){
 
 // Others
 
+function clearDisplay(){
+    prev_num = ''.toString();
+    temp_num = ''.toString();
+    dotBtn.disabled = false;
+    prevNumDisplay.textContent = '';
+    currNumDisplay.textContent = '0';
+
+}
+
 function equals(){
     const ops = {
       "+": (x, y) => x + y,
@@ -60,6 +83,9 @@ function equals(){
     prev_num = parseFloat(prev_num, 10);
     temp_num = parseFloat(temp_num, 10);
     let result = ops[operation](prev_num, temp_num);
+    prevNumDisplay.textContent = '';
+    currNumDisplay.textContent = result;
+
     prev_num = '';
     temp_num = '';
     dotBtn.disabled = false;
